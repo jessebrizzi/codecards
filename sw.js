a74b0099fe21d781373baa04a8739310a6237d4c
Jesse Brizzi <jessebrizzi@gmail.com>
2025-01-01 21:00:34 -0500

    support installing as pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'codecards-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json',
+    '/imgs/16.png',
+    '/imgs/32.png',
+    '/imgs/96.png',
+    '/imgs/180.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
